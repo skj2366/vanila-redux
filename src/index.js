1083a0d0661fd5d8d28c064c65714076dc940cda
DongKeun, Kim <skj2366@naver.com>
2020-03-18 04:52:33 +0900

    3

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+const countModifier = (count = 0) => {
+    return count;
+};
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () =>{
-    number.innerText = count;
-}
-
-const handleAdd = () => {
-    count ++;
-    updateText();
-}
-
-const handleMinus = () => {
-    count --;
-    updateText();
-}
-
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+const countStore = createStore(countModifier);
+console.log(countStore.getState());
